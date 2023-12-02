27673b4bfd83346306a2848b4b7697be7b15fd4c
たけちゃん <79554611+takechan-NITNC@users.noreply.github.com>
2023-12-03 02:32:32 +0900

    Roomのテストを記述

---
+++ room_test.ts
@@ -0,0 +1,10 @@
+import { assertEquals } from "https://deno.land/std@0.65.0/testing/asserts.ts";
+import { Person } from "./person.ts";
+import { Room } from "./room.ts"
+Deno.test({
+	name: "constructor",
+	fn: function() {
+		const got = new Room(new Set([new Person("人間１"), new Person("人間２"), new Person("人間３")]));
+		assertEquals(got.members, new Set([new Person("人間１"), new Person("人間２"), new Person("人間３")]));
+	}
+});
