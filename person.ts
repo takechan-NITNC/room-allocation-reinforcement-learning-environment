f9a08345a592931c524271b5ef795016eba7ee14
たけちゃん <79554611+takechan-NITNC@users.noreply.github.com>
2023-12-04 22:00:26 +0900

    同室希望・同室拒否の矛盾や，自分自身に対する設定への対策を強化

--- person.ts
+++ person.ts
@@ -1,4 +1,5 @@
 import { ContradictoryImpressionsError } from "./contradictory_impressions_error.ts";
+import { SelfImpressionError } from "./self_impression_error.ts";
 export class Person {
   readonly name: string;
   readonly likedPeople = new Set<Person>();
@@ -7,18 +8,28 @@ export class Person {
     this.name = name;
   }
   likes(...people: Person[]): void {
+    if (people.includes(this)) {
+      throw new SelfImpressionError(this);
+    }
     for (const person of people) {
       if (this.dislikedPeople.has(person)) {
         throw new ContradictoryImpressionsError(this, person);
       }
+    }
+    for (const person of people) {
       this.likedPeople.add(person);
     }
   }
   dislikes(...people: Person[]): void {
+    if (people.includes(this)) {
+      throw new SelfImpressionError(this);
+    }
     for (const person of people) {
       if (this.likedPeople.has(person)) {
         throw new ContradictoryImpressionsError(this, person);
       }
+    }
+    for (const person of people) {
       this.dislikedPeople.add(person);
     }
   }
